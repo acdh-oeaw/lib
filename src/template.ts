ef747ad13605c4a20742d12f9996d602c40c58b4
Stefan Probst <stefanprobst@posteo.de>
2024-04-16 19:40:52 +0200

    fix: fix types

--- src/template.ts
+++ src/template.ts
@@ -3,9 +3,9 @@ import { get } from "./get.js";
 const regex = /{(.+?)}/g;
 
 export function template(value: string, values: Record<string, unknown>): string {
-	return value.replace(regex, (match, capture) => {
+	return value.replace(regex, (match, capture: string) => {
 		const replaced = get(values, capture);
-		if (typeof replaced === "string" || typeof replaced === "number") return replaced;
+		if (typeof replaced === "string" || typeof replaced === "number") return String(replaced);
 		return match;
 	});
 }
