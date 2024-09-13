80ab44d1eb181bcdfc0d67c548c4dea89ea07959
Stefan Probst <stefanprobst@posteo.de>
2024-09-13 10:06:25 +0200

    chore: bump node, commitlint, add eslint-config-node

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
+/** @typedef {import("typescript-eslint").Config} Config */
+
 import base from "@acdh-oeaw/eslint-config";
+import node from "@acdh-oeaw/eslint-config-node";
 import gitignore from "eslint-config-flat-gitignore";
 
-const config = [gitignore(), ...base];
+/** @type {Config} */
+const config = [gitignore(), ...base, ...node];
 
 export default config;
