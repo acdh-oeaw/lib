b09f0f8e91797098283cd911ef0f8fc408b3f5f1
Stefan Probst <stefanprobst@posteo.de>
2026-01-06 11:42:58 +0100

    chore: let oxfmt handle import sorting (#43)

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,4 +3,15 @@ import node from "@acdh-oeaw/eslint-config-node";
 import gitignore from "eslint-config-flat-gitignore";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig(gitignore({ strict: false }), base, node);
+export default defineConfig(
+	gitignore({ strict: false }),
+	base,
+	{
+		/** Import sorting is handled by `oxfmt`. */
+		rules: {
+			"simple-import-sort/imports": "off",
+			"simple-import-sort/exports": "off",
+		},
+	},
+	node,
+);
