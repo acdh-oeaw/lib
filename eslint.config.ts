3f59e497ee75dda9dfd38a9ac8796ec941cf3b9f
Stefan Probst <stefanprobst@posteo.de>
2026-01-06 11:39:29 +0100

    chore: update, format

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
 import base from "@acdh-oeaw/eslint-config";
 import node from "@acdh-oeaw/eslint-config-node";
-import { defineConfig } from "eslint/config";
 import gitignore from "eslint-config-flat-gitignore";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig(gitignore({ strict: false }), base, node);
